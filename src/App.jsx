f0a90b7a8b1fe9c1ae1e784b66d1c2961d59e515
SaidARSLAN <lost.myreligion@hotmail.com>
2023-09-14 12:07:56 +0300

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import './App.css'
+import LeftSideBar from './components/LeftSideBar'
+import MainSection from './components/MainSection'
+import RightSideBar from './components/RightSideBar'
+
 
 function App() {
 
   return (
     <main className='grid grid-cols-3 container mx-auto mt-12 px-9'>
       <section>
-        LEFT SECTION
+        <LeftSideBar />
       </section>
       <section>
-        MAIN SECTION
+        <MainSection />
       </section>
       <section>
-        RIGHT SECTION
+        <RightSideBar />
       </section>
     </main>
   )
