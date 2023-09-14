88fd5089ae9339a9dd4de904a92c966b64be5edf
SaidARSLAN <lost.myreligion@hotmail.com>
2023-09-14 12:00:16 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='grid grid-cols-3 container mx-auto mt-12 px-9'>
+      <section>
+        LEFT SECTION
+      </section>
+      <section>
+        MAIN SECTION
+      </section>
+      <section>
+        RIGHT SECTION
+      </section>
+    </main>
+  )
+}
+
+export default App
