757259b570d67bf53dafc1a9dc3d5a72e47ad784
SaidARSLAN <lost.myreligion@hotmail.com>
2023-09-18 11:24:42 +0300

    Filtered section has done

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -7,7 +7,12 @@ import { NavLink } from "react-router-dom"
 const LeftSideBar = () => {
   const [search, setSearch] = useState("");
 
-
+  const handleCloseDropDown = () => {
+    if (searchRef.current.classList.contains("flex")) {
+      searchRef.current.classList.remove("flex");
+      searchRef.current.classList.add("hidden");
+    }
+  }
 
   const handleDropDown = () => {
       if(document.querySelector("#dropdown").classList.contains("flex")) {
@@ -20,20 +25,13 @@ const LeftSideBar = () => {
       }
   } 
   const searchRef = useRef();
-  // const handleSearch = () => {
-  //   if (searchRef.current.classList.contains("hidden")) {
-  //     searchRef.current.classList.remove("hidden");
-  //     searchRef.current.classList.add("flex");
-  //   }
+  const handleSearch = () => {
+     if (searchRef.current.classList.contains("hidden")) {
+       searchRef.current.classList.remove("hidden");
+       searchRef.current.classList.add("flex");
+     }
     
-  // }
- 
-  // document.body.addEventListener("click",(event) => {
-  //   if (event.target.id !== "dontpush" && searchRef.current.classList.contains("flex")) {
-  //     searchRef.current.classList.remove("flex");
-  //     searchRef.current.classList.add("hidden");
-  //   }
-  // })
+    }
 
   const afterFilteredUsers = anotherUsers.filter((user) => user.username.includes(search));
   const handleFilter = (event) => {
@@ -57,8 +55,9 @@ const LeftSideBar = () => {
         </div>
         <div className="flex-1 flex items-center justify-center space-x-6">
           <div className="flex flex-col relative">
-          <input placeholder="Search" className="px-4 py-1 rounded-lg w-[240px] bg-gray-200" value={search} onChange={handleFilter}  id="dontpush"></input>
+          <input placeholder="Search" className="px-4 py-1 rounded-lg w-[240px] bg-gray-200" value={search} onChange={handleFilter} onClick={handleSearch} id="dontpush"></input>
           <ul className="absolute top-10 ml-2 hidden flex-col space-y-2 bg-gray-200 w-full rounded-lg px-2 py-2" ref={searchRef}>
+            <div className="w-full flex items-end justify-end pr-2 text-x cursor-pointer text-gray-500  hover:text-black duration-300 font-bold" onClick={handleCloseDropDown}><p>X</p></div>
             {afterFilteredUsers.map((user,idx) => {
                 return (<li key={idx} className="flex space-x-2 cursor-pointer hover:bg-gray-500 duration-200 rounded-lg"><img src={user.photo} className="w-10 h-10 rounded-full" /><p className="text-lg text-center mt-1">{user.username}</p></li>)
             })}
