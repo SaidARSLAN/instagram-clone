065f0ac5b196cb3387c4534385e7dbe7dba2ee9a
SaidARSLAN <lost.myreligion@hotmail.com>
2023-09-20 17:47:25 +0300

    rightsidebar has been fixed

--- src/components/RightSideBar.jsx
+++ src/components/RightSideBar.jsx
@@ -6,31 +6,31 @@ const RightSideBar = () => {
     <div  className='absolute right-16 hidden lg:flex lg:flex-col space-y-4'>
       <div className='flex w-full space-x-24 items-center'>
         <div className='flex'>
-          <img src={userone} className='w-[50px] rounded-full'/>
-        <div>
-        <h3>saidarslanq</h3>
-        <p>saidarslanq</p>
+          <img src={userone} className='w-[50px] rounded-full border-[2px] border-gray-500 cursor-pointer'/>
+        <div className='pl-4'>
+        <h3 className='text-sm'><strong>saidarslanq</strong></h3>
+        <p className='text-sm'>saidarslanq</p>
         </div>
         </div>
         <div>
-          <p>Change Account</p>
+          <p className='text-blue-600 cursor-pointer text-xs font-bold'>Change Account</p>
         </div>
       </div>
       <div className='flex w-full space-x-24 items-center'>
-        <p>Recommendation for you</p>
-        <p>See All</p>
+        <p className='text-xs font-bold text-gray-500'>Recommendation for you</p>
+        <p className='text-xs font-bold'>See All</p>
       </div>
-      <div>
+      <div className='flex flex-col items-center justify-center'>
         {anotherUsers.map((user) => {
-            return<div className='flex space-x-24'>  
+            return<div className='flex space-x-24 items-center justify-center'>  
             <div className='flex gap-4 my-4'>
-              <img src={user.photo}  className='w-[50px] rounded-full'/>
+              <img src={user.photo}  className='w-[40px] rounded-full'/>
               <div className='flex flex-col'>
-              <p>{user.username}</p>
-            <p>Following you</p>
+              <p className='text-xs font-bold'>{user.username}</p>
+            <p className='text-xs'>Following you</p>
             </div>
             </div>
-            <p>Follow</p>
+            <p className='text-center bg-blue-500 text-white px-4 py-2 rounded-lg cursor-pointer'>Follow</p>
             </div>
         })}
       </div>
